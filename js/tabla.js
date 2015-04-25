48cd30fa040426a0865fb05ab416c6d7abb283a7
Alex Ballera <alexballera@gmail.com>
2015-04-24 23:53:21 -0430

    Terminado!

--- js/tabla.js
+++ js/tabla.js
@@ -1,58 +1,112 @@
+// Referencias:
+// http://stackoverflow.com/questions/26552749/best-way-to-iterate-array-of-hashes-javascript
+// http://stackoverflow.com/questions/18333427/how-to-insert-row-in-html-table-body-in-javascript
+
+
+var personas = new Array();
+var keys = new Array();
+var values = new Array();
+
 function definirEstructura() {
-    arreglo = new Array();
-    arreglo.push({
-        "cedula": "123456",
-        "nombre": "Pepito",
-        "apellido": "Pérez",
-        "correo": "pepiperez@hotmail.com"
-    });
-    arreglo.push({
-        "cedula": "234567",
-        "nombre": "Andres",
-        "apellido": "Alvarez",
-        "correo": "andresalvarez@hotmail.com"
-    });
-    return arreglo;
+    persona1 = {
+        "Cedula": "123456",
+        "Nombre": "Pepito",
+        "Apellido": "Pérez",
+        "Correo": "pepiperez@hotmail.com"
+    };
+    personas.push(persona1);
+    persona2 = {
+        "Cedula": "234567",
+        "Nombre": "Andres",
+        "Apellido": "Alvarez",
+        "Correo": "andresalvarez@hotmail.com"
+    };
+    personas.push(persona2);
+    return definirEstructura;
 }
-var a = definirEstructura();
-console.log(a);
 
-
-function genera_tabla() {
-    // Obtener la referencia del elemento body
-    var body = document.getElementsByTagName("body")[0];
-
-    // Crea un elemento <table> y un elemento <tbody>
-    var tabla = document.createElement("table");
-
-    var tblBody = document.createElement("tbody");
-
-    // Crea las celdas
-    for (var i = 0; i < 2; i++) {
-        // Crea las hileras de la tabla
-        var hilera = document.createElement("tr");
-
-        for (var j = 0; j < 4; j++) {
-            // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-            // texto sea el contenido de <td>, ubica el elemento <td> al final
-            // de la hilera de la tabla
-            // AQUÍ HACER UN FOR PARA RECORRER EL ARREGLO Y LO IMPRIMA EN LA TABLA
-            for (key in a) {
-                console.log(a);
-                var celda = document.createElement("td");
-                var textoCelda = document.createTextNode("test");
-                celda.appendChild(textoCelda);
-                hilera.appendChild(celda);
-            }
+function generarThead(i){
+    for (var values in personas[i]) {
+        if (personas[i].hasOwnProperty(values)) {
+            values.push(values);
+            values.push(personas[key][i]);
         }
-        // agrega la hilera al final de la tabla (al final del elemento tblbody)
-        tblBody.appendChild(hilera);
     }
-
-    // posiciona el <tbody> debajo del elemento <table>
-    tabla.appendChild(tblBody);
-    // appends <table> into <body>
-    body.appendChild(tabla);
-    // modifica el atributo "border" de la tabla y lo fija a "2";
-    tabla.setAttribute("border", "4");
+    //console.log(keys);
 }
+
+function generarTabla(i){
+    for (var key in personas[i]) {
+        if (personas[i].hasOwnProperty(key)) {
+            keys.push(key);
+            values.push(personas[i][key]);
+        }
+    }
+    //console.log(values);
+}
+generarThead(0)
+definirEstructura();
+generarTabla(0);
+
+var tabla = document.getElementsByTagName("thead")[0];
+
+var linea = tabla.insertRow(tabla.rows.length);
+
+var celda = linea.insertCell(0);
+var texto = document.createTextNode(keys[0]);
+celda.appendChild(texto);
+
+var celda = linea.insertCell(1);
+var texto = document.createTextNode(keys[1]);
+celda.appendChild(texto);
+
+var celda = linea.insertCell(2);
+var texto = document.createTextNode(keys[2]);
+celda.appendChild(texto);
+
+var celda = linea.insertCell(3);
+var texto = document.createTextNode(keys[3]);
+celda.appendChild(texto);
+
+generarTabla(1);
+
+var tabla = document.getElementsByTagName("tbody")[0];
+
+var linea = tabla.insertRow(tabla.rows.length);
+
+var celda = linea.insertCell(0);
+var texto = document.createTextNode(values[0]);
+celda.appendChild(texto);
+
+var celda = linea.insertCell(1);
+var texto = document.createTextNode(values[1]);
+celda.appendChild(texto);
+
+var celda = linea.insertCell(2);
+var texto = document.createTextNode(values[2]);
+celda.appendChild(texto);
+
+var celda = linea.insertCell(3);
+var texto = document.createTextNode(values[3]);
+celda.appendChild(texto);
+
+generarTabla(1);
+
+var linea = tabla.insertRow(tabla.rows.length);
+
+var celda = linea.insertCell(0);
+var texto = document.createTextNode(values[4]);
+celda.appendChild(texto);
+
+var celda = linea.insertCell(1);
+var texto = document.createTextNode(values[5]);
+celda.appendChild(texto);
+
+var celda = linea.insertCell(2);
+var texto = document.createTextNode(values[6]);
+celda.appendChild(texto);
+
+var celda = linea.insertCell(3);
+var texto = document.createTextNode(values[7]);
+celda.appendChild(texto);
+
